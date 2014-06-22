0e772ab56710fc54d8917ec6e9b5c7de577bdd75
jschwertz <jschwertz@sbcglobal.net>
2014-06-22 15:31:30 -0500

    JFS01

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
-## Assignment 2 of R Programming:
+sq## Assignment 2 of R Programming:
+##
 ## The first function creates a special "matrix" (actually a list with a matrix
 ## and a function) and caches (writes) its inverse to the parent function, or
 ## global environment.  The second function computes the inverse of the special
@@ -10,23 +11,26 @@
 ## global environment.
 
 makeCacheMatrix <- function(x = matrix()) {
-    ## initialize variables in this environment
+    
+    ## initialize variable in this environment
     m <- NULL
+    
+    ## initialize variables in the parent environment
     set <- function(y) {
-        ## initialize variables in the parent environment
         x <<- y
         m <<- NULL
     }
+    
+    ## define "functions"
     get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
     
     ## return the special "matrix"
     list (set = set, get = get,
-          setmean = setmean,
-          getmean = getmean)
+          setsolve = setsolve,
+          getsolve = getsolve)
 }
-
 
 
 ## This function computes the inverse of the special "matrix".  If the matrix 
@@ -34,13 +38,25 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-    m <- x$getmenan()
+    
+    ## intialize variables
+    m <- x$getsolve()
+    
+    ## check for cached "matrix"
     if(!is.null(m)) {
         message("getting cached data")
         return(m)
     }
+    
+    ## cache doesn't exist calculate inverse
+    
+    ## initialize variables
     data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
+    m <- solve(data, ...)
+    
+    ## calculate inverse
+    x$setsolve(m)
+    
+    ## return newly calculated "matrix"
     m
 }
