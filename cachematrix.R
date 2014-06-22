19c2c1d8ea67528eca3ca0b5218da90e7b0945cb
jschwertz <jschwertz@sbcglobal.net>
2014-06-22 15:31:30 -0500

    JFS01

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
 ## Assignment 2 of R Programming:
+##
 ## The first function creates a special "matrix" (actually a list with a matrix
 ## and a function) and caches (writes) its inverse to the parent function, or
 ## global environment.  The second function computes the inverse of the special
@@ -10,13 +11,17 @@
 ## global environment.
 
 makeCacheMatrix <- function(x = matrix()) {
-    ## initialize variables in this environment
+    
+    ## initialize variable in this environment
     m <- NULL
+    
+    ## initialize variables in the parent environment
     set <- function(y) {
-        ## initialize variables in the parent environment
         x <<- y
         m <<- NULL
     }
+    
+    ## define "functions"
     get <- function() x
     setmean <- function(mean) m <<- mean
     getmean <- function() m
@@ -28,19 +33,30 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-
 ## This function computes the inverse of the special "matrix".  If the matrix 
 ## has already been calculated then the function returns the cached "matrix".
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
+    
+    ## intialize variables
     m <- x$getmenan()
+    
+    ## check for cached "matrix"
     if(!is.null(m)) {
         message("getting cached data")
         return(m)
     }
+    
+    ## cache doesn't exist calculate inverse
+    
+    ## initialize variables
     data <- x$get()
     m <- mean(data, ...)
+    
+    ## calculate inverse
     x$setmean(m)
+    
+    ## return newly calculated "matrix"
     m
 }
